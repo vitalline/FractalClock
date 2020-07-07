ca6ec20ecbefa79a3d157319ef6a1b4151975160
playday3008 <titanv3585@gmail.com>
2020-07-07 13:29:37 +0200

    Better code struct

--- FractalClock/Main.cpp
+++ FractalClock/Main.cpp
@@ -63,8 +63,10 @@ static bool draw_clock = true;
 static bool is_fullscreen = false;
 static bool show_shortcuts = true;
 
-struct Res {
-  Res(int id) {
+struct Res
+{
+  Res(int id)
+  {
     HRSRC src = ::FindResource(NULL, MAKEINTRESOURCE(id), RT_RCDATA);
     ptr = ::LockResource(::LoadResource(NULL, src));
     size = (size_t)::SizeofResource(NULL, src);
@@ -73,11 +75,13 @@ struct Res {
   size_t size;
 };
 
-void FractalIterMS(sf::Vector2f pt, sf::Vector2f dir, int depth) {
+void FractalIterMS(sf::Vector2f pt, sf::Vector2f dir, int depth)
+{
   const sf::Color& col = color_scheme[depth];
-  if (depth == 0) {
+  if (depth == 0)
     point_array.emplace_back(pt, col);
-  } else {
+  else
+  {
     const sf::Vector2f dirS((dir.x*rotS.x - dir.y*rotS.y)*ratioS, (dir.y*rotS.x + dir.x*rotS.y)*ratioS);
     const sf::Vector2f dirM((dir.x*rotM.x - dir.y*rotM.y)*ratioM, (dir.y*rotM.x + dir.x*rotM.y)*ratioM);
     FractalIterMS(pt + dirS, dirS, depth - 1);
@@ -89,11 +93,13 @@ void FractalIterMS(sf::Vector2f pt, sf::Vector2f dir, int depth) {
   }
 }
 
-void FractalIterHMS(sf::Vector2f pt, sf::Vector2f dir, int depth) {
+void FractalIterHMS(sf::Vector2f pt, sf::Vector2f dir, int depth)
+{
   const sf::Color& col = color_scheme[depth];
-  if (depth == 0) {
+  if (depth == 0)
     point_array.emplace_back(pt, col);
-  } else {
+  else
+  {
     const sf::Vector2f dirS((dir.x*rotS.x - dir.y*rotS.y)*ratioS, (dir.y*rotS.x + dir.x*rotS.y)*ratioS);
     const sf::Vector2f dirM((dir.x*rotM.x - dir.y*rotM.y)*ratioM, (dir.y*rotM.x + dir.x*rotM.y)*ratioM);
     const sf::Vector2f dirH((dir.x*rotH.x - dir.y*rotH.y)*ratioH, (dir.y*rotH.x + dir.x*rotH.y)*ratioH);
@@ -109,11 +115,13 @@ void FractalIterHMS(sf::Vector2f pt, sf::Vector2f dir, int depth) {
   }
 }
 
-void FractalIterHM(sf::Vector2f pt, sf::Vector2f dir, int depth) {
+void FractalIterHM(sf::Vector2f pt, sf::Vector2f dir, int depth)
+{
   const sf::Color& col = color_scheme[depth];
-  if (depth == 0) {
+  if (depth == 0)
     point_array.emplace_back(pt, col);
-  } else {
+  else
+  {
     const sf::Vector2f dirM((dir.x*rotM.x - dir.y*rotM.y)*ratioM, (dir.y*rotM.x + dir.x*rotM.y)*ratioM);
     const sf::Vector2f dirH((dir.x*rotH.x - dir.y*rotH.y)*ratioH, (dir.y*rotH.x + dir.x*rotH.y)*ratioH);
     FractalIterHM(pt + dirM, dirM, depth - 1);
@@ -125,7 +133,8 @@ void FractalIterHM(sf::Vector2f pt, sf::Vector2f dir, int depth) {
   }
 }
 
-INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nShowCmd) {
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nShowCmd)
+{
   //GL settings
   sf::ContextSettings settings;
   settings.depthBits = 24;
@@ -189,36 +198,46 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
   //Main Loop
   clock.restart();
-  while (window.isOpen()) {
+  while (window.isOpen())
+  {
     sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) {
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
         window.close();
         break;
-      } else if (event.type == sf::Event::KeyPressed) {
+      }
+      else if (event.type == sf::Event::KeyPressed)
+      {
         #pragma warning(disable:26812)
         const sf::Keyboard::Key keycode = event.key.code;
         #pragma warning(default:26812)
-        if (keycode == sf::Keyboard::Escape) {
+        if (keycode == sf::Keyboard::Escape)
+        {
           window.close();
           break;
-        } else if (keycode == sf::Keyboard::M) {
+        }
+        else if (keycode == sf::Keyboard::M)
           clock_type = ClockType((clock_type + 1) % ClockType::NUM);
-        } else if (keycode == sf::Keyboard::R) {
+        else if (keycode == sf::Keyboard::R)
+        {
           use_realtime = !use_realtime;
           clock.restart();
-        } else if (keycode == sf::Keyboard::T) {
-          use_tick = !use_tick;
-        } else if (keycode == sf::Keyboard::B) {
-          draw_branches = !draw_branches;
-        } else if (keycode == sf::Keyboard::C) {
-          draw_clock = !draw_clock;
-        } else if (keycode == sf::Keyboard::F11) {
-          toggle_fullscreen = true;
-        } else if (keycode == sf::Keyboard::K) {
-            show_shortcuts = !show_shortcuts;
         }
-      } else if (event.type == sf::Event::Resized) {
+        else if (keycode == sf::Keyboard::T)
+          use_tick = !use_tick;
+        else if (keycode == sf::Keyboard::B)
+          draw_branches = !draw_branches;
+        else if (keycode == sf::Keyboard::C)
+          draw_clock = !draw_clock;
+        else if (keycode == sf::Keyboard::F11)
+          toggle_fullscreen = true;
+        else if (keycode == sf::Keyboard::K)
+            show_shortcuts = !show_shortcuts;
+      }
+      else if (event.type == sf::Event::Resized)
+      {
         screenSize.width = event.size.width;
         screenSize.height = event.size.height;
         window.setView(sf::View(sf::FloatRect(0.0f, 0.0f, (float)screenSize.width, (float)screenSize.height)));
@@ -227,13 +246,13 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     //Calculate maximum iterations
     int iters = max_iters;
-    if (clock_type == ClockType::HMS) {
+    if (clock_type == ClockType::HMS)
       iters = max_iters - 3;
-    }
 
     //Get the time
     float cur_time = 0.0f;
-    if (use_realtime) {
+    if (use_realtime)
+    {
       FILETIME fileTime;
       SYSTEMTIME systemTime;
       SYSTEMTIME localTime;
@@ -244,10 +263,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
       cur_time += float(localTime.wSecond);
       cur_time += float(localTime.wMinute) * 60.0f;
       cur_time += float(localTime.wHour) * 3600.0f;
-    } else {
-      cur_time = clock.getElapsedTime().asSeconds();
     }
-    if (use_tick) {
+    else
+      cur_time = clock.getElapsedTime().asSeconds();
+    if (use_tick)
+    {
       static const float a = 30.0f;
       static const float b = 14.0f;
       const float x = std::fmodf(cur_time, 1.0f);
@@ -272,7 +292,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     //Update the clock face
     clock_face_array1.clear();
     clock_face_array2.clear();
-    for (int i = 0; i < 60; ++i) {
+    for (int i = 0; i < 60; ++i)
+    {
       const float ang = float(i) * 2.0f * PI / 60.0f;
       const sf::Vector2f v(std::cos(ang), std::sin(ang));
       const bool is_hour = (i % 5 == 0);
@@ -286,17 +307,21 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     const float r1 = std::sin(cur_time * 0.017f)*0.5f + 0.5f;
     const float r2 = std::sin(cur_time * 0.011f)*0.5f + 0.5f;
     const float r3 = std::sin(cur_time * 0.003f)*0.5f + 0.5f;
-    for (int i = 0; i < iters; ++i) {
+    for (int i = 0; i < iters; ++i)
+    {
       const float a = float(i) / float(iters - 1);
       const float h = std::fmodf(r2 + 0.5f*a, 1.0f);
       const float s = 0.5f + 0.5f * r3 - 0.5f*(1.0f - a);
       const float v = 0.3f + 0.5f * r1;
-      if (i == 0) {
+      if (i == 0)
+      {
         color_scheme[i] = FromHSV(h, 1.0f, 1.0f);
         color_scheme[i].a = 128;
-      } else if (i == iters - 1 && draw_clock) {
+      }
+      else if (i == iters - 1 && draw_clock)
         color_scheme[i] = clock_face_color;
-      } else {
+      else
+      {
         color_scheme[i] = FromHSV(h, s, v);
         color_scheme[i].a = 255; //128;
       }
@@ -305,28 +330,27 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     //Update the fractal
     line_array.clear();
     point_array.clear();
-    if (clock_type == ClockType::HM) {
+    if (clock_type == ClockType::HM)
       FractalIterHM(pt, dir, iters - 1);
-    } else if (clock_type == ClockType::HMS) {
+    else if (clock_type == ClockType::HMS)
       FractalIterHMS(pt, dir, iters - 1);
-    } else if (clock_type == ClockType::MS) {
+    else if (clock_type == ClockType::MS)
       FractalIterMS(pt, dir, iters - 1);
-    }
 
     //Clear the screen
     window.clear(bgnd_color);
 
     //Draw the fractal branches
-    if (draw_branches) {
+    if (draw_branches)
+    {
       glEnable(GL_LINE_SMOOTH);
       glLineWidth(2.0f);
-      if (!draw_clock) {
+      if (!draw_clock)
         window.draw(line_array.data(), line_array.size(), sf::PrimitiveType::Lines);
-      } else if (clock_type == ClockType::HMS) {
+      else if (clock_type == ClockType::HMS)
         window.draw(line_array.data(), line_array.size() - 6, sf::PrimitiveType::Lines);
-      } else {
+      else
         window.draw(line_array.data(), line_array.size() - 4, sf::PrimitiveType::Lines);
-      }
     }
 
     //Draw the final fractal in a brighter color
@@ -335,7 +359,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     window.draw(point_array.data(), point_array.size(), sf::PrimitiveType::Points);
 
     //Draw the clock
-    if (draw_clock) {
+    if (draw_clock)
+    {
       //Draw the clock face lines
       glEnable(GL_LINE_SMOOTH);
       glLineWidth(4.0f);
@@ -344,7 +369,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
       window.draw(clock_face_array2.data(), clock_face_array2.size(), sf::PrimitiveType::Lines);
     
       //Draw the clock face numbers
-      for (int i = 0; i < 12; ++i) {
+      for (int i = 0; i < 12; ++i)
+      {
         const float ang = float(i) * 2.0f * PI / 12.0f;
         const sf::Vector2f v(std::sin(ang), -std::cos(ang));
         const std::string num_str = std::to_string(i == 0 ? 12 : i);
@@ -358,19 +384,24 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
       }
 
       //Draw the clock hands
-      if (clock_type == ClockType::HM) {
+      if (clock_type == ClockType::HM)
+      {
         glLineWidth(4.0f);
         window.draw(line_array.data() + line_array.size() - 4, 2, sf::PrimitiveType::Lines);
         glLineWidth(5.0f);
         window.draw(line_array.data() + line_array.size() - 2, 2, sf::PrimitiveType::Lines);
-      } else if (clock_type == ClockType::HMS) {
+      }
+      else if (clock_type == ClockType::HMS)
+      {
         glLineWidth(2.0f);
         window.draw(line_array.data() + line_array.size() - 6, 2, sf::PrimitiveType::Lines);
         glLineWidth(4.0f);
         window.draw(line_array.data() + line_array.size() - 4, 2, sf::PrimitiveType::Lines);
         glLineWidth(5.0f);
         window.draw(line_array.data() + line_array.size() - 2, 2, sf::PrimitiveType::Lines);
-      } else if (clock_type == ClockType::MS) {
+      }
+      else if (clock_type == ClockType::MS)
+      {
         glLineWidth(2.0f);
         window.draw(line_array.data() + line_array.size() - 4, 2, sf::PrimitiveType::Lines);
         glLineWidth(4.0f);
@@ -379,7 +410,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 
     //Draw UI elements
-    if (show_shortcuts) {
+    if (show_shortcuts)
+    {
         clock_type_text.setString(clock_type_name[clock_type]);
         window.draw(clock_type_text);
         realtime_text.setString(realtime_name[use_realtime ? 1 : 0]);
@@ -400,14 +432,18 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     window.display();
 
     //Toggle full-screen if needed
-    if (toggle_fullscreen) {
+    if (toggle_fullscreen)
+    {
       toggle_fullscreen = false;
       is_fullscreen = !is_fullscreen;
-      if (is_fullscreen) {
+      if (is_fullscreen)
+      {
         window.close();
         screenSize = sf::VideoMode::getDesktopMode();
         window.create(screenSize, "Fractal Clock", sf::Style::Fullscreen, settings);
-      } else {
+      }
+      else
+      {
         window.close();
         screenSize = sf::VideoMode(window_w_init, window_h_init, 24);
         window.create(screenSize, "Fractal Clock", sf::Style::Resize | sf::Style::Close, settings);
@@ -415,6 +451,5 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
       window.setMouseCursorVisible(!is_fullscreen);
     }
   }
-
   return 0;
 }
